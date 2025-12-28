b25971c0e84fb83abee6b9039e43ba4248aa1e5d
imabhishekmahli <abhi8h3k@gmail.com>
2025-12-28 10:11:28 +0530

    reading variables

---
+++ Lab_2/reading_variables.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int x,y,z;
+  cout<<"Enter three integers: ";
+  cin>>x>>y>>z;
+  cout<<"You entered: "<<x<<", "<<y<<", "<<z<<endl;
+
+  return 0;
+}
