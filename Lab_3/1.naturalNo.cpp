228374d21ab12b8f8e8b9ed8fdc6ac40c780248b
imabhishekmahli <abhi8h3k@gmail.com>
2025-12-29 11:52:56 +0530

    natural from 1 to N, reverse the digit, 1 to N print all the odd

--- Lab_3/1.naturalNo.cpp
+++ Lab_3/1.naturalNo.cpp
@@ -9,11 +9,36 @@ int natual(int x)
   }
 }
 
+int reverse(int n)
+{
+  int rev=0;
+  while(n>0)
+  {
+    int last = n%10;
+    rev = rev*10 +last;
+    n=n/10;
+  }
+  return rev;
+}
+
+int odd(int z)
+{
+  for(int i=1;i<=z;i++)
+  {
+    if(i%2!=0)
+    {
+      cout<<i<<" ";
+    }
+  }
+}
 int main()
 {
   int a;
   cin>>a;
-  cout<<natual(a);
-
+  //cout<<natual(a);
+  cout<<endl;
+  //cout<<reverse(a);
+  cout<<endl;
+  cout<<odd(a);
   return 0;
 }
