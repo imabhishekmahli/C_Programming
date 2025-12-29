fea56fd561bc73a057d7fc65cf99f4ef024a7e0c
imabhishekmahli <abhi8h3k@gmail.com>
2025-12-29 12:27:47 +0530

    pattern 2

---
+++ Lab_4/3.pattern.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+void pattern1(int x)
+{
+  for(int i=1;i<=x;i++)
+  {
+    for(int j=1;j<=x;j++)
+    {
+      cout<<" * ";
+    }
+    cout<<endl;
+  }
+}
+
+void pattern2(int x)
+{
+  for(int i=1;i<=x;i++)
+  {
+    for(int j=1;j<=i;j++)
+    {
+      cout<<" * ";
+    }
+    cout<<endl;
+  }
+}
+int main()
+{
+  int n;
+  cin>>n;
+  //pattern1(n);
+  pattern2(n);
+  return 0;
+}
