e78eee46738afa1e049d62c643c5590691b16ab3
imabhishekmahli <abhi8h3k@gmail.com>
2025-12-29 13:50:43 +0530

    pattern 5

--- Lab_4/3.pattern.cpp
+++ Lab_4/3.pattern.cpp
@@ -48,6 +48,18 @@ void pattern4(int x)
     cout<<endl;
   }
 }
+
+void pattern5(int x)
+{
+  for(int i=1;i<=x;i++)
+  {
+    for(int j=1;j<=i;j++)
+    {
+      cout<<j<<" ";
+    }
+    cout<<endl;
+  }
+}
 int main()
 {
   int n;
@@ -55,6 +67,7 @@ int main()
   //pattern1(n);
   //pattern2(n);
   //pattern3(n);
-  pattern4(n);
+  //pattern4(n);
+  pattern5(n);
   return 0;
 }
