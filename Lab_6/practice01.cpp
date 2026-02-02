54badf35e9400e738daf5821b1bc05d67de3dec7
imabhishekmahli <abhi8h3k@gmail.com>
2026-02-02 22:10:44 +0530

    practice

---
+++ Lab_6/practice01.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  // char ch = 'A';
+  // cout<<ch<<endl;
+  // cout<<int(ch)<<endl;
+  // ch = ch + 1;
+  // cout<<ch<<endl;
+  // cout<<int(ch)<<endl;
+  // ch = ch+24;
+  // cout<<ch<<endl;
+  // cout<<int(ch)<<endl;
+
+  for(char ch='A';ch<='Z';ch++)
+  {
+    cout<<ch<<" "<<int(ch)<<endl;
+  }
+  return 0;
+}
