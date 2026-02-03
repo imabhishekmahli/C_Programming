e7c2500ad9ae73f3acc24dab7c8c05a5ded40e7b
imabhishekmahli <abhi8h3k@gmail.com>
2026-02-03 09:47:24 +0530

    address in string

--- Lab_7/practice01.cpp
+++ Lab_7/practice01.cpp
@@ -7,17 +7,19 @@ int main()
   // cout<<"value in a :"<<a<<endl;
   // cout<<"address of a :"<<&a<<endl;
 
-  int arr[10];
-  cout<<arr[0]<<endl;
-  cout<<&arr[0]<<endl;
-  cout<<arr<<endl;
-  cout<<arr+1<<endl; //integer takes 4 bits
-  cout<<arr+2<<endl;
+  // int arr[10];
+  // cout<<arr[0]<<endl;
+  // cout<<&arr[0]<<endl;
+  // cout<<arr<<endl;
+  // cout<<arr+1<<endl; //integer takes 4 bits
+  // cout<<arr+2<<endl;
 
-  string a[10];
-  cout<<a<<endl;
-  cout<<&a<<endl;
-  cout<<&a+1<<endl;
-  cout<<&a+2<<endl;
+  string name = "abhishek kumar";
+  cout<<name<<endl;
+  cout<<&name<<endl;
+  cout<<&name+1<<endl;
+  cout<<&name+2<<endl;
+  cout<<name[1]<<endl;
+  cout<<&name[4]<<endl;
   return 0;
 }
