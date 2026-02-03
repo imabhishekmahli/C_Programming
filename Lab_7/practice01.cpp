581bd78937caed3bd7a513bb9cc7e01b13a80283
imabhishekmahli <abhi8h3k@gmail.com>
2026-02-03 09:43:53 +0530

    value and address

--- Lab_7/practice01.cpp
+++ Lab_7/practice01.cpp
@@ -3,8 +3,21 @@ using namespace std;
 
 int main()
 {
-  int a = 10;
-  cout<<"value in a :"<<a<<endl;
-  cout<<"address of a :"<<&a<<endl;
+  // int a = 10;
+  // cout<<"value in a :"<<a<<endl;
+  // cout<<"address of a :"<<&a<<endl;
+
+  int arr[10];
+  cout<<arr[0]<<endl;
+  cout<<&arr[0]<<endl;
+  cout<<arr<<endl;
+  cout<<arr+1<<endl; //integer takes 4 bits
+  cout<<arr+2<<endl;
+
+  string a[10];
+  cout<<a<<endl;
+  cout<<&a<<endl;
+  cout<<&a+1<<endl;
+  cout<<&a+2<<endl;
   return 0;
 }
